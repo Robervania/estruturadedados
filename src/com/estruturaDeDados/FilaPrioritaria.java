03204b0acc194925f80288fb089e75183bf687de
Robervania <vania25@protonmail.com>
2022-08-22 23:22:42 -0300

    Fila Prioritaria

---
+++ src/com/estruturaDeDados/FilaPrioritaria.java
@@ -0,0 +1,32 @@
+package com.estruturaDeDados;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class FilaPrioritaria {
+
+    public static void main(String[] args) {
+
+        // Fila em ordem reversa
+        //Queue<Double> fila = new PriorityQueue<>(Collections.reverseOrder());
+
+        //Fila em ordem padrão.
+        Queue<Double> fila = new PriorityQueue<>();
+
+        // Para inverter a ordem da Fila
+        //Queue<Double> fila = new PriorityQueue<>(Collections.reverseOrder());
+
+        fila.offer(6.0);
+        fila.offer(7.0);
+        fila.offer(6.5);
+        fila.offer(5.2);
+        fila.offer(9.0);
+        fila.offer(8.0);
+
+        while(!fila.isEmpty()){
+            System.out.println(fila.poll());
+        }
+    }
+
+}
